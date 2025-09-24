08a4b5326d0a34d3b890aafd8f3ac8e4a4f2dfe3
johnatticus <johnahysong@gmail.com>
2025-09-24 16:11:14 -0400

    initial commit

---
+++ web/themes/junebug_theme/js/junebug-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * junebug_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.junebugTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
